9957d37297a39a1cf90f8ad6e99dcdae86ca1bb6
JoongHyun-Kim <80838501+JoongHyun-Kim@users.noreply.github.com>
2021-07-28 21:59:49 +0900

    Ch 3.4.2.3 useState

--- react-techniques/src/Say.js
+++ react-techniques/src/Say.js
@@ -5,11 +5,43 @@ const Say = () => {
    const onClickEnter = () => setMessage('안녕하세요!');
    const onClickLeave = () => setMessage('안녕히 가세요!');
 
+   const [color, setColor] = useState('black');
+
    return (
       <div>
          <button onClick={onClickEnter}>입장</button>
          <button onClick={onClickLeave}>퇴장</button>
-         <h1>{message}</h1>
+         <h1 style={{ color }}>{message}</h1>
+         <button
+            style={{ backgroundColor: 'black', color: 'white' }}
+            onClick={() => setColor('black')}
+         >
+            Black
+         </button>
+         <button
+            style={{ backgroundColor: 'red', color: 'white' }}
+            onClick={() => setColor('red')}
+         >
+            Red
+         </button>
+         <button
+            style={{ backgroundColor: 'green', color: 'white' }}
+            onClick={() => setColor('green')}
+         >
+            Green
+         </button>
+         <button
+            style={{ backgroundColor: 'blue', color: 'white' }}
+            onClick={() => setColor('blue')}
+         >
+            Blue
+         </button>
+         <button
+            style={{ backgroundColor: 'purple', color: 'white' }}
+            onClick={() => setColor('purple')}
+         >
+            Purple
+         </button>
       </div>
    );
 };
