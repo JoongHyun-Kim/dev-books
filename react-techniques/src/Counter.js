8634cc83b419e04d131d0d57b42a79442d4857bf
JoongHyun-Kim <80838501+JoongHyun-Kim@users.noreply.github.com>
2021-07-28 21:33:18 +0900

    Ch 3.4.1.3 this.setState에 함수를 인자로 전달

--- react-techniques/src/Counter.js
+++ react-techniques/src/Counter.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         //state 초기값 설정
-         number: 0,
-      };
-   }
+   state = {
+      number: 0,
+      fixedNumber: 0,
+   };
    render() {
-      const { number } = this.state; //state를 조회할 때는 this.state로 조회
+      const { number, fixedNumber } = this.state; //state를 조회할 때는 this.state로 조회
       return (
          <div>
             <h1>{number}</h1>
+            <h2>바뀌지 않는 값: {fixedNumber}</h2>
             <button
                onClick={() => {
-                  this.setState({ number: number + 1 }); //setState를 이용해 state에 새로운 값 설정
-               }}
+                  this.setState((prevState) => {
+                     //this.setState()에 객체 대신 함수를 인자로 넣어주기
+                     return {
+                        number: prevState.number + 1,
+                     };
+                  });
+                  this.setState((prevState) => ({
+                     number: prevState.number + 1,
+                  })); //화살표함수에서 바로 객체를 반환하도록 prevState => ({ })형태
+               }} //+1 버튼 한 번 누를 때마다 number값 2씩 증가
             >
                +1
             </button>
