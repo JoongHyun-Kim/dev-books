5dbe609dd7ad1ccae3061ab8026ca3e1dd75dcb5
JoongHyun Kim <joongh@ewhain.net>
2021-08-01 13:46:58 +0900

    Ch 4.2.2.2 Update state from input

--- react-techniques/src/EventPractice.js
+++ react-techniques/src/EventPractice.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
+   state = {
+      //state 초기값 설정
+      message: '',
+   };
+
    render() {
       return (
          <div>
             <h1>이벤트 연습</h1>
+            <input
+               type="text"
+               name="message"
+               placeholder="입력"
+               value={this.state.message}
+               onChange={(e) => {
+                  this.setState({
+                     //state값 업데이트
+                     message: e.target.value,
+                  });
+               }}
+            />
          </div>
       );
    }
