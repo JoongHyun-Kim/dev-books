2bf3b6152f5a70f6a52a934a167df552a0cdd29c
JoongHyun Kim <joongh@ewhain.net>
2021-11-02 21:12:50 +0900

    Create readNumber.kt

---
+++ KotlinInAction/Ch02/readNumber.kt
@@ -0,0 +1,12 @@
+fun readNumber(reader: BufferedReader) : Int? {
+  try {
+    val line = reader.readLine()
+    return Integer.parseInt(line)
+  }
+  catch (e: NumberFormatException) {
+    return null
+  }
+  finally {
+    reader.close()
+  }
+}
