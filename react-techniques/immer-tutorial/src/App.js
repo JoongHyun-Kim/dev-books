b1b3675a5a9d86fa5f4bfdedca6d5bd33aa880f3
JoongHyun Kim <joongh@ewhain.net>
2021-12-24 23:16:27 +0900

    Update App.js using usestate and immer

--- react-techniques/immer-tutorial/src/App.js
+++ react-techniques/immer-tutorial/src/App.js
@@ -1,70 +1,64 @@
 import React, { useRef, useCallback, useState } from 'react';
 import produce from 'immer';
-
+ 
 const App = () => {
-   const nextId = useRef(1);
-   const [form, setForm] = useState({ name: '', username: '' });
-   const [data, setData] = useState({
-      array: [],
-      uselessValue: null,
-   });
-
-   // input 수정을 위한 함수
-   const onChange = useCallback(
-      (e) => {
-         const { name, value } = e.target;
-         setForm(
-            produce((form, draft) => { //immer
-               draft[name] = value;
-            })
-         );
-      },
-      [form]
-   );
-
-   // form 등록을 위한 함수
-   const onSubmit = useCallback(
-      (e) => {
-         e.preventDefault();
-         const info = {
-            id: nextId.current,
-            name: form.name,
-            username: form.username,
-         };
-
-         // array에 새 항목 등록
-         setData(
-            produce((data, draft) => { //immer
-               draft.array.push(info);
-            })
-         );
-
-         // form 초기화
-         setForm({
-            name: '',
-            username: '',
-         });
-         nextId.current += 1;
-      },
-      [data, form.name, form.username]
-   );
-
-   // 항목을 삭제하는 함수
-   const onRemove = useCallback(
-      (id) => {
-         setData(
-            produce((data, draft) => { //immer
-               draft.array.splice(
-                  draft.array.findIndex((info) => info.id === id),
-                  1
-               );
-            })
-         );
-      },
-      [data]
-   );
-
-   return (
+  const nextId = useRef(1);
+  const [form, setForm] = useState({ name: '', username: '' });
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null
+  });
+ 
+  // input 수정을 위한 함수
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    setForm(
+      produce(draft => { //파라미터를 함수 형태로 사용
+        draft[name] = value;
+      })
+    );
+  }, []);
+ 
+  // form 등록을 위한 함수
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username
+      };
+ 
+      // array에 새 항목 등록
+      setData(
+        produce(draft => { //파라미터를 함수 형태로 사용 
+          draft.array.push(info);
+        })
+      );
+ 
+      // form 초기화
+      setForm({
+        name: '',
+        username: ''
+      });
+      nextId.current += 1;
+    },
+    [form.name, form.username]
+  );
+ 
+  // 항목을 삭제하는 함수
+  const onRemove = useCallback(
+    id => {
+      setData(
+        produce(draft => { //파라미터를 함수 형태로 사용
+          draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
+        })
+      );
+    },
+    []
+  );
+ 
+  return (
       <div>
          <form onSubmit={onSubmit}>
             <input
@@ -93,5 +87,5 @@ const App = () => {
       </div>
    );
 };
-
+ 
 export default App;
