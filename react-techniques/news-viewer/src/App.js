c31c0fdef3bcca7be2463301a5cc06a42e2a4178
JoongHyun Kim <joongh@ewhain.net>
2022-01-06 01:03:41 +0900

    Update App.js

--- react-techniques/news-viewer/src/App.js
+++ react-techniques/news-viewer/src/App.js
@@ -1,26 +1,8 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
-   const [data, setData] = useState(null);
-   const onClick = async () => {
-      try {
-         const response = await axios.get(
-            'https://newsapi.org/v2/top-headlines?country=kr&apiKey=9a930de1c908403bb469d8a6bfd57068'
-         );
-         setData(response.data);
-      } catch (e) {
-         console.log(e);
-      }
-   };
-   return (
-      <div>
-         <div>
-            <button onClick={onClick}>불러오기</button>
-         </div>
-         {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
-      </div>
-   );
+   return <NewsList />;
 };
 
 export default App;
